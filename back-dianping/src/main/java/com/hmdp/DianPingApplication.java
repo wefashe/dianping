935713fe4649b50905d23bc7d914459baf53759c
wefashe <wefashe@qq.com>
2023-04-11 12:18:06 +0800

    后端代码

---
+++ back-dianping/src/main/java/com/hmdp/DianPingApplication.java
@@ -0,0 +1,13 @@
+package com.hmdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DianPingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DianPingApplication.class, args);
+    }
+
+}
