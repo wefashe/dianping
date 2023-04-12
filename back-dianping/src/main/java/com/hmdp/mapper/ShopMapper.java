845f91dfc8b831e00ad98c1b4a2bd4c0c791e583
wefashe <wefashe@qq.com>
2023-04-12 14:39:01 +0800

    fix

--- back-dianping/src/main/java/com/hmdp/mapper/ShopMapper.java
+++ back-dianping/src/main/java/com/hmdp/mapper/ShopMapper.java
@@ -5,7 +5,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
- * Shop BlogMapper
+ * Shop Mapper 接口
  */
 @Mapper
 public interface ShopMapper extends BaseMapper<Shop> {
